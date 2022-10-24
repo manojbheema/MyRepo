ef57eeb72d30291ddefc27d24ffd92d60d7e311d
manojbheema <manojbheema@gmail.com>
2022-10-24 20:56:21 +0530

    Test

--- com/home/test/TestList.java
+++ com/home/test/TestList.java
@@ -16,5 +16,6 @@ public class TestList {
         stringList.removeAll(strings);
         boolean b = stringList.isEmpty();
         System.out.println(b);
+        System.out.println(stringList);
     }
 }
