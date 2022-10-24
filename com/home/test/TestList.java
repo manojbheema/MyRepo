ea8f63d8bdde73b3d802b30ca5f297260e2fd47f
manojbheema <manojbheema@gmail.com>
2022-10-24 20:48:50 +0530

    Test
    Test

---
+++ com/home/test/TestList.java
@@ -0,0 +1,20 @@
+package com.home.test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestList {
+    public static void main(String[] args) {
+        List<String> stringList = new ArrayList<>();
+        stringList.add("test");
+        stringList.add("test2");
+
+        List<String> strings = new ArrayList<>();
+        strings.add("test1");
+        strings.add("test2");
+
+        stringList.removeAll(strings);
+        boolean b = stringList.isEmpty();
+        System.out.println(b);
+    }
+}
